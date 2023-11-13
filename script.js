70aaefece00740fca75e221a7d149297470e27a3
Henrique Heron <henriqueheron1@gmail.com>
2023-11-13 19:02:22 -0300

    feat: add event listeners that change the display-result paragraph

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 // math functions
 function add(...a) {
     return a.reduce((accum, value) => accum + value);
@@ -25,6 +27,7 @@ let secondNumber = 0;
 let operator = '';
 
 
+// Will make this into an event listener for when the user hits '='
 function operate(a, b, operator) {
     if (operator === '+') {
         return add(a, b);
@@ -40,3 +43,76 @@ function operate(a, b, operator) {
     }
 }
 
+// Event listeners
+    const numpad = document.querySelector('#numpad');
+    const result = document.querySelector('#result');
+
+    numpad.addEventListener('click', (e) => {
+        let pressedKey = e.target;
+
+        switch(pressedKey.textContent) {
+
+            case '(':
+                result.textContent += '(';
+                break;
+            case ')':
+                result.textContent += ')';
+                break;
+            case '%':
+                result.textContent += '%';
+                break;
+            case 'AC':
+                result.textContent = '';
+                break;
+            case '1':
+                result.textContent += 1;
+                break;
+            case '2':
+                result.textContent += 2;
+                break;
+            case '3':
+                result.textContent += 3;
+                break;
+            case '/':
+                result.textContent += '/';
+                break;
+            case '4':
+                result.textContent += 4;
+                break;
+            case '5':
+                result.textContent += 5;
+                break;
+            case '6':
+                result.textContent += 6;
+                break;
+            case 'x':
+                result.textContent += '*';
+                break;
+            case '7':
+                result.textContent += 7;
+                break;
+            case '8':
+                result.textContent += 8;
+                break;
+            case '9':
+                result.textContent += 9;
+                break;
+            case '-':
+                result.textContent += '-';
+                break;
+            case '0':
+                result.textContent += 0;
+                break;
+            case '.':
+                result.textContent += '.';
+                break;
+            case '=':
+                // result.textContent = ... 
+                break;
+            case '+':
+                result.textContent += '+';
+                break;
+
+        }
+    })
+})
