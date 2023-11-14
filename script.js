35b339d3a076ffaddbb0c38e25186e0453966c82
Henrique Heron <henriqueheron1@gmail.com>
2023-11-13 21:58:28 -0300

    feat: do math calculations

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
+// operation builder
+let firstNumber;
+let secondNumber;
+let operator = '';
+let output = undefined;
 
 // math functions
-function add(...a) {
-    return a.reduce((accum, value) => accum + value);
+function add() {
+    return firstNumber + secondNumber;
 }
 
-function subtract(...a) {
-    return a.reduce((accum, value) => accum - value);
+function subtract() {
+    return firstNumber - secondNumber;
 }
 
-function multiply(...a) {
-    return a.reduce((accum, value) => accum * value);
+function multiply() {
+    return firstNumber * secondNumber;
 }
 
-function divide(...a) {
-    return a.reduce((accum, value) => accum / value);
+function divide() {
+    return  firstNumber / secondNumber;
 }
 
 
 let test = add(10, 2, 2, 5)
 console.log(test);
 
-// operation builder
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = '';
-
 
 // Will make this into an event listener for when the user hits '='
-function operate(a, b, operator) {
-    if (operator === '+') {
-        return add(a, b);
+function operate() {
+    if (typeof output === 'undefined') {
+
+        if (operator === '+') {
+            return add(firstNumber, secondNumber);
+        }
+        if (operator === '-') {
+            return subtract(firstNumber, secondNumber);
+        }
+        if (operator === '*') {
+            return multiply(firstNumber, secondNumber);
+        }
+        if (operator === '/') {
+            return divide(firstNumber, secondNumber);
+        }
     }
-    if (operator === '-') {
-        return subtract(a, b);
-    }
-    if (operator === '*') {
-        return multiply(a, b);
-    }
-    if (operator === '/') {
-        return divide(a, b);
-    }
+    
+        if (operator === '+') {
+            return output = add(output, firstNumber);
+        }
+        if (operator === '-') {
+            output = subtract(output, firstNumber);
+        }
+        if (operator === '*') {
+            output = multiply(output, firstNumber);
+        }
+        if (operator === '/') {
+            output = divide(output, firstNumber);
+        }
 }
+
+function clearCalculator() {
+     firstNumber = null;
+     secondNumber = null;
+     operator = null;
+     output = null;
+     result.textContent = '';
+}     
 
 // Event listeners
     const numpad = document.querySelector('#numpad');
     const result = document.querySelector('#result');
 
-    numpad.addEventListener('click', (e) => {
+    numpad.addEventListener('click', (e) => { // for display only
         let pressedKey = e.target;
 
         switch(pressedKey.textContent) {
@@ -62,57 +86,119 @@ function operate(a, b, operator) {
                 result.textContent += '%';
                 break;
             case 'AC':
-                result.textContent = '';
+                clearCalculator();
                 break;
             case '1':
                 result.textContent += 1;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 1;
+                } else {
+                    secondNumber = 1;
+                }
                 break;
             case '2':
                 result.textContent += 2;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 2;
+                } else {
+                    secondNumber = 2;
+                }
                 break;
             case '3':
                 result.textContent += 3;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 3;
+                } else {
+                    secondNumber = 3;
+                }
                 break;
             case '/':
                 result.textContent += '/';
+                operator = '/';
                 break;
             case '4':
                 result.textContent += 4;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 4;
+                } else {
+                    secondNumber = 4;
+                }
                 break;
             case '5':
                 result.textContent += 5;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 5;
+                } else {
+                    secondNumber = 5;
+                }
                 break;
             case '6':
                 result.textContent += 6;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 6;
+                } else {
+                    secondNumber = 6;
+                }
                 break;
             case 'x':
                 result.textContent += 'x';
+                operator = '*';
                 break;
             case '7':
                 result.textContent += 7;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 7;
+                } else {
+                    secondNumber = 7;
+                }
                 break;
             case '8':
                 result.textContent += 8;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 8;
+                } else {
+                    secondNumber = 8;
+                }
                 break;
             case '9':
                 result.textContent += 9;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 9;
+                } else {
+                    secondNumber = 9;
+                }
                 break;
             case '-':
                 result.textContent += '-';
+                operator = '-';
                 break;
             case '0':
                 result.textContent += 0;
+                if (typeof firstNumber !== 'number') {
+                    firstNumber = 0;
+                } else {
+                    secondNumber = 0;
+                }
                 break;
             case '.':
                 result.textContent += '.';
                 break;
-            case '=':
-                // result.textContent = ... 
-                break;
+        
             case '+':
                 result.textContent += '+';
+                operator = '+';
                 break;
 
+            case '=':
+                output = operate();
+                result.textContent = output;
+                firstNumber = null;
+                secondNumber = null;
+                break;
         }
+        console.log(`First Number: ${firstNumber}`);
+        console.log(`Second Number: ${secondNumber}`);
+        console.log(`Operator: ${operator}`);
+        console.log(`Output: ${output}`);
     })
 })
