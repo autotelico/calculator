e1a55e8d69cad6a547b033759058ab9270d492e1
Henrique Heron <henriqueheron1@gmail.com>
2023-11-22 19:27:23 -0300

    feat: add multiple number operations (pt 1)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 // operation builder
-let firstNumber;
+let firstNumber = 0;
 let operator = '';
-let output;
+let output = 0;
 
 // math functions
 function add(a, b) {
@@ -22,11 +22,6 @@ function divide(a, b) {
 }
 
 
-let test = add(10, 2, 2, 5)
-console.log(test);
-
-
-// An event listener for when the user hits '='
 function operate() {
         if (operator === '+') {
             return add(output, firstNumber);
@@ -56,139 +51,159 @@ function clearCalculator() {
     numpad.addEventListener('click', (e) => { // for display only
         let pressedKey = e.target;
 
-        if (typeof output === 'number' && typeof firstNumber === 'number') {
-            output = operate();
-            firstNumber = null;
+        if (pressedKey.textContent === '+' ||
+        pressedKey.textContent === '-' ||
+        pressedKey.textContent === '*' ||
+        pressedKey.textContent === '/') {
+            operator = pressedKey.textContent;
         }
 
-        if (operator == null) {
-            output += pressedKey.textContent;
-            output = parseInt(output);
-            console.log('addition happened');
+        if (!operator) {
+            if (pressedKey.textContent !== '+' ||
+            pressedKey.textContent !== '-' ||
+            pressedKey.textContent !== '*' ||
+            pressedKey.textContent !== '/') {
+                if (output) {
+                    output = output.concat(pressedKey.textContent);
+                    console.log('concatenated');
+                } else {
+                    output = pressedKey.textContent;
+                }
+            }
         }
 
 
-        switch(pressedKey.textContent) {
 
-            case '(':
-                result.textContent += '(';
-                break;
-            case ')':
-                result.textContent += ')';
-                break;
-            case '%':
-                result.textContent += '%';
-                break;
-            case 'AC':
-                clearCalculator();
-                break;
-            case '1':
-                result.textContent += 1;
-                if (typeof output !== 'number') {
-                    output = 1;
-                } else {
-                    firstNumber = 1;
-                }
-                break;
-            case '2':
-                result.textContent += 2;
-                if (typeof output !== 'number') {
-                    output = 2;
-                } else {
-                    firstNumber = 2;
-                }
-                break;
-            case '3':
-                result.textContent += 3;
-                if (typeof output !== 'number') {
-                    output = 3;
-                } else {
-                    firstNumber = 3;
-                }
-                break;
-            case '/':
-                result.textContent += '/';
-                operator = '/';
-                break;
-            case '4':
-                result.textContent += 4;
-                if (typeof output !== 'number') {
-                    output = 4;
-                } else {
-                    firstNumber = 4;
-                }
-                break;
-            case '5':
-                result.textContent += 5;
-                if (typeof output !== 'number') {
-                    output = 5;
-                } else {
-                    firstNumber = 5;
-                }
-                break;
-            case '6':
-                result.textContent += 6;
-                if (typeof output !== 'number') {
-                    output = 6;
-                } else {
-                    firstNumber = 6;
-                }
-                break;
-            case 'x':
-                result.textContent += 'x';
-                operator = '*';
-                break;
-            case '7':
-                result.textContent += 7;
-                if (typeof output !== 'number') {
-                    output = 7;
-                } else {
-                    firstNumber = 7;
-                }
-                break;
-            case '8':
-                result.textContent += 8;
-                if (typeof output !== 'number') {
-                    output = 8;
-                } else {
-                    firstNumber = 8;
-                }
-                break;
-            case '9':
-                result.textContent += 9;
-                if (typeof output !== 'number') {
-                    output = 9;
-                } else {
-                    firstNumber = 9;
-                }
-                break;
-            case '-':
-                result.textContent += '-';
-                operator = '-';
-                break;
-            case '0':
-                result.textContent += 0;
-                if (typeof output !== 'number') {
-                    output = 0;
-                } else {
-                    firstNumber = 0;
-                }
-                break;
-            case '.':
-                result.textContent += '.';
-                break;
+        // if (typeof output === 'number' && typeof firstNumber === 'number') {
+        //     output = operate();
+        //     firstNumber = null;
+        // }
+
+        if (pressedKey.textContent === 'AC') {
+            clearCalculator();
+        }
+
+        // switch(pressedKey.textContent) {
+
+        //     case '(':
+        //         result.textContent += '(';
+        //         break;
+        //     case ')':
+        //         result.textContent += ')';
+        //         break;
+        //     case '%':
+        //         result.textContent += '%';
+        //         break;
+        //     case 'AC':
+        //         clearCalculator();
+        //         break;
+        //     case '1':
+        //         result.textContent += 1;
+        //         if (typeof output !== 'number') {
+        //             output = 1;
+        //         } else {
+        //             firstNumber = 1;
+        //         }
+        //         break;
+        //     case '2':
+        //         result.textContent += 2;
+        //         if (typeof output !== 'number') {
+        //             output = 2;
+        //         } else {
+        //             firstNumber = 2;
+        //         }
+        //         break;
+        //     case '3':
+        //         result.textContent += 3;
+        //         if (typeof output !== 'number') {
+        //             output = 3;
+        //         } else {
+        //             firstNumber = 3;
+        //         }
+        //         break;
+        //     case '/':
+        //         result.textContent += '/';
+        //         operator = '/';
+        //         break;
+        //     case '4':
+        //         result.textContent += 4;
+        //         if (typeof output !== 'number') {
+        //             output = 4;
+        //         } else {
+        //             firstNumber = 4;
+        //         }
+        //         break;
+        //     case '5':
+        //         result.textContent += 5;
+        //         if (typeof output !== 'number') {
+        //             output = 5;
+        //         } else {
+        //             firstNumber = 5;
+        //         }
+        //         break;
+        //     case '6':
+        //         result.textContent += 6;
+        //         if (typeof output !== 'number') {
+        //             output = 6;
+        //         } else {
+        //             firstNumber = 6;
+        //         }
+        //         break;
+        //     case 'x':
+        //         result.textContent += 'x';
+        //         operator = '*';
+        //         break;
+        //     case '7':
+        //         result.textContent += 7;
+        //         if (typeof output !== 'number') {
+        //             output = 7;
+        //         } else {
+        //             firstNumber = 7;
+        //         }
+        //         break;
+        //     case '8':
+        //         result.textContent += 8;
+        //         if (typeof output !== 'number') {
+        //             output = 8;
+        //         } else {
+        //             firstNumber = 8;
+        //         }
+        //         break;
+        //     case '9':
+        //         result.textContent += 9;
+        //         if (typeof output !== 'number') {
+        //             output = 9;
+        //         } else {
+        //             firstNumber = 9;
+        //         }
+        //         break;
+        //     case '-':
+        //         result.textContent += '-';
+        //         operator = '-';
+        //         break;
+        //     case '0':
+        //         result.textContent += 0;
+        //         if (typeof output !== 'number') {
+        //             output = 0;
+        //         } else {
+        //             firstNumber = 0;
+        //         }
+        //         break;
+        //     case '.':
+        //         result.textContent += '.';
+        //         break;
         
-            case '+':
-                result.textContent += '+';
-                operator = '+';
-                break;
+        //     case '+':
+        //         result.textContent += '+';
+        //         operator = '+';
+        //         break;
 
-            case '=':
-                result.textContent = output;
+        //     case '=':
+        //         result.textContent = output;
                 
-                firstNumber = null;
-                break;
-        }
+        //         firstNumber = null;
+        //         break;
+        // }
 
         
 
