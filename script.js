4350ce72c9d229f7f5abd11f3900db1becb98439
Henrique Heron <henriqueheron1@gmail.com>
2023-11-25 15:20:18 -0300

    refactor: switch back to previous working version

--- script.js
+++ script.js
@@ -50,19 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
     numpad.addEventListener('click', (e) => { // for display only
         let pressedKey = e.target;
 
-        if (operator && output && firstNumber) {
-            console.log('All fields are true');
-            if (pressedKey.textContent === '+' ||
-            pressedKey.textContent === '-' ||
-            pressedKey.textContent === 'x' ||
-            pressedKey.textContent === '/' ||
-            pressedKey.textContent === '=') {
-                output = operate();
-                return operator = '';
-            }
-
-        }
-
         if (operator) {
             if (pressedKey.textContent === '+' ||
                 pressedKey.textContent === '-' ||
@@ -100,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 pressedKey.textContent !== 'x' ||
                 pressedKey.textContent !== '/') {
                 if (output) {
-                    console.log(`firstNumber is ${firstNumber}`);
+                    console.log(`Output is ${output}`);
                     if (typeof output !== 'number') {
                         console.error('Error: output is ' + typeof firstNumber);
                     }
